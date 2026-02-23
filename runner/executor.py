a6a503978018862c068986b1ffb0854e299f4467
PYatiM <p.yathinmachaiah18@gmail.com>
2026-02-23 21:21:47 +0530

    fixed cross platform executor issues

--- runner/executor.py
+++ runner/executor.py
@@ -1,21 +1,23 @@
 import subprocess
 import tempfile
 import os
+import sys
 
-
+preexec = set_limits if os.name != "nt" else None
 def execute_python(code: str, timeout: int = 2):
     with tempfile.TemporaryDirectory() as tmpdir:
         file_path = os.path.join(tmpdir, "user_code.py")
 
         with open(file_path, "w") as f:
             f.write(code)
-
+        
         try:
             result = subprocess.run(
-                ["python3", file_path],
+                [sys.executable, file_path],
                 capture_output=True,
                 text=True,
-                timeout=timeout
+                timeout=timeout,
+                preexec_fn=preexec
             )
 
             return {
