f333c0a798f9d89bfa43b19ca13c270ab8615836
PYatiM <p.yathinmachaiah18@gmail.com>
2026-02-25 20:31:16 +0530

    Fast api website working hurray, now strengttning it to prevent any exploitation

--- api/app.py
+++ api/app.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from runner.executor import execute_python
+import asyncio
 
 app = FastAPI()
-
+MAX_CONCURRENT_EXECUTIONS = 2
+semaphore = asyncio.Semaphore(MAX_CONCURRENT_EXECUTIONS)
 
 class CodeRequest(BaseModel):
     code: str
 
 
 @app.post("/execute")
-def run_code(request: CodeRequest):
-    return execute_python(request.code)
+async def run_code(request: CodeRequest):
+    async with semaphore:
+        loop = asyncio.get_event_loop()
+        result = await loop.run_in_executor(
+            None,
+            execute_python,
+            request.code
+        )
+        return result
