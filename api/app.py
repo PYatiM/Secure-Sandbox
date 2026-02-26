ec1151802370abd41e5557f513103718c959bf9e
PYatiM <p.yathinmachaiah18@gmail.com>
2026-02-26 20:04:18 +0530

    Added basic Rate limiting (An IP throttle)

--- api/app.py
+++ api/app.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi import Request, HTTPException
 from runner.executor import execute_python
+from collections import defaultdict
+import time
 import asyncio
+
+RATE_LIMIT = 10  # Max requests per minute
+WINDOW_SECONDS = 60
+requests_log = defaultdict(list)
+
+def is_rate_limited(client_ip):
+    now = time.time()
+    requests_log[client_ip] = [t for t in requests_log[client_ip] if now - t < WINDOW_SECONDS]
+    
+    if len(requests_log[client_ip]) >= RATE_LIMIT:
+        return True
+    requests_log[client_ip].append(now)
+    return False
 
 app = FastAPI()
 MAX_CONCURRENT_EXECUTIONS = 2
@@ -10,9 +26,13 @@ semaphore = asyncio.Semaphore(MAX_CONCURRENT_EXECUTIONS)
 class CodeRequest(BaseModel):
     code: str
 
-
 @app.post("/execute")
-async def run_code(request: CodeRequest):
+async def run_code(request: CodeRequest, req: Request):
+    client_ip = req.client.host
+
+    if is_rate_limited(client_ip):
+        raise HTTPException(status_code=429, detail="Rate limit exceeded")
+
     async with semaphore:
         loop = asyncio.get_event_loop()
         result = await loop.run_in_executor(
