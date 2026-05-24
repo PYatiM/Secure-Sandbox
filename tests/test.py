e2a14f8a35ab7b982ef44506e1ab9945e77567db
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2026-05-24 13:34:04 +0000

    deleted the main cause it had no developemental purpose and improved the test file instead

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,11 @@
 from runner.executor import execute_python
 
-if __name__ == "__main__":
-    user_code = "print('Hello Sandbox')"
-    result = execute_python(user_code)
-    print(result)
+def basic_test():
+    result = execute_python("print('Hello Sandbox')")
+    assert result["returncode"] == 0
+    assert "Hello Sandbox" int result["stdout"]
+
+def test_timeout():
+    result = execute_python("while True: pass")
+    assert result["returncode"] == -1
+    assert "timed out" in result["stderr"].lower()
